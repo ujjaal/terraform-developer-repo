49e604f23b4f2fe716c3d2dce949d183e425ae59
ujjaalchanda <ujjaalchanda@gmail.com>
2024-11-24 01:17:07 +0530

    updated terraform.tf file

--- avm-labs/part01-base/terraform.tf
+++ avm-labs/part01-base/terraform.tf
@@ -1,29 +1,20 @@
 terraform {
-  required_version = "~> 1.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.105"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~>3.0"
     }
   }
   backend "azurerm" {
-      resource_group_name  = "platform-rg"  
+      resource_group_name  = "platform-rg"
       storage_account_name = "saplatterraformstate"
       container_name       = "terraform-state"
       key                  = "terraform.tfstate"
   }
+
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false # This is to handle MCAPS or other policy driven resource creation.
-    }
-  }
-  # Use Managed Identity for authentication
-  use_msi = true
+  features {}
 }
+
